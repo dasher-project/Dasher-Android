af498ab51254c4cfb48a8cefb9d83dabcf303f98
will wade <willwade@gmail.com>
2026-08-22 09:34:29 +0000

    chore: bump versionName/versionCode to 0.1.8
    Aligns the in-app version line (RFC 0016, Settings -> Privacy) with the
    v0.1.8 release tag; versionName had stayed at 0.1.0 since inception,
    which would have made the displayed version permanently wrong.
    
    Signed-off-by: will wade <willwade@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "at.dasher.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 8
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
